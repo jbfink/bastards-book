aa74a5b6b0d3e519a7ae4ff496387d6ac29acd05
jbfink <john.fink@gmail.com>
2012-05-27 23:09:12 -0400

    Scarf?

--- scarf.rb
+++ scarf.rb
@@ -1,10 +1,10 @@
 require "open-uri"
 
 remote_base_url = "http://en.wikipedia.org/wiki"
-remote_page_name = "Dennis_Ritchie"
+remote_page_name = "Steve_Wozniak"
 remote_full_url = remote_base_url + "/" + remote_page_name
 
-puts "Downloading from: " + remote_full_url
+
 
 somedata = open(remote_full_url).read
 my_local_filename = "my_copy_of-" + remote_page_name + ".html"
@@ -15,3 +15,6 @@ my_local_file = open(my_local_filename, "w")
 my_local_file.write(somedata)
 my_local_file.close
 
+file_for_reading = open(my_local_filename, "r")
+ puts file_for_reading.read
+file_for_reading.close
